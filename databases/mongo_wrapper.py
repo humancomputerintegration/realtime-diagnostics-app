02b0e0239936e3bd46c64e53404691bd7ae2c973
Ted Kim <tedkim97@gmail.com>
2019-06-30 23:29:48 -0500

    pseudo code/outline for work tomorrow

---
+++ databases/mongo_wrapper.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+def open_connection():
+	client = MongoClient('mongodb://loclhost:27017')
+
+def pick_db(db_name):
+
+def get_collection(collection_name):
+
+def insert_document():
+
+def find_document(query requirements):
+ 	
