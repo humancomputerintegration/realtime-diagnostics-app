21d54bddaf7e198277baadd1f12d7f82d4d3a297
Ted <tedkim97@gmail.com>
2019-07-07 18:22:29 -0500

    more work done on the mongowrapper

--- databases/mongo_wrapper.py
+++ databases/mongo_wrapper.py
@@ -1,10 +1,10 @@
+#Pymongo wrapper to help further abstract database construction in MongoDB
 import pymongo
 from pprint import pprint 
-
 from pymongo.uri_parser import parse_host 
 
 #https://api.mongodb.com/python/current/api/pymongo/database.html
-# Things to implement
+
 
 #create_collection, more robust collection creation 
 #This method should only be used to specify options on creation
@@ -17,7 +17,6 @@ from pymongo.uri_parser import parse_host
 #db.command("buildinfo")
 #db.command("collstats", collection_name)
 #db.command("filemd5", object_id, root=file_root) 
-
 
 #Client Functions 
 def open_connection(host:str, port:int, username, password):
@@ -38,31 +37,59 @@ def open_connection(host:str, port:int, username, password):
 def close_connection(client):
 	client.close()
 
-#Database Functions
-def createUser(database, username, password, hierarchy="user",roles=["read"]):
-	database.command("createUser", hierarchy, pwd = password, roles=roles)
-
-def updateUser(database, username, password, hierarchy="user",roles=["read"]):
-	datbase.command("updateUser", hierarchy, pwd = password, roles=roles)
-
-def drop_db(client,database_name):
-	client.drop_db(database_name)
-
-
-# def get_create_db:
-
-# def get_db():
+#Database & User Functions
 
 def list_dbs(client, mode = 'names'):
 	valid_lists = {'names': client.list_database_names(),
 					'cursors': client.list_databases()}
-	throw_error = lambda : ValueError('Not a mode')
-	return valid_lists.get(mode, throw_error)
+	return valid_lists.get(mode, ValueError('Input mode is not valid'))
+
+# Creates and returns a new DB 
+# Note: A database will not be created until there is at 
+# least one record in the DB - so we create a dummy collection
+def create_db(client, new_database):
+	if new_database in client.list_database_names():
+		print("Database already exists -- returning none")
+		return None 
+	temp_coll = client[new_database].testcollection.insert_one({'DUMMY':0})
+	print("finished creating database::", new_database)
+	return client[new_database]
+
+#checks if a DB exists and returns it
+def get_db(client, database_name):
+	if not (database_name in client.list_database_names()):
+		print("database is not in client - function ending")
+		return None;
+	print("returning ::", database_name)
+	return client[database_name]
+
+def drop_db(client,database_name):
+	if not (database_name in client.list_database_names()):
+		print("database is not in the client - function ending")
+		return;
+	print("dropping db :: ", database_name)
+	client.drop_database(database_name)
+	print(client.list_database_names())
+	return;
 	
+def createUser(database, username, password, hierarchy="user",roles=["read"]):
+	database.command("createUser", hierarchy, pwd = password, roles=roles)
+	print("Created {} with {} and {} roles".format(username, hierarchy, roles))
+
+def updateUser(database, username, password, hierarchy="user",roles=["read"]):
+	datbase.command("updateUser", hierarchy, pwd = password, roles=roles)
+	print("updated user")
+
+
+def insert_record(client, database_name, collection_name, records):
+	if(records == None):
+
+
+def create_collection(client, database_name, new_collection):
+
 # def get_collection(collection_name):
 
 # def insert_document():
-
 # def find_document(query requirements):
  
 def unit_test():
@@ -76,13 +103,18 @@ if __name__ == "__main__":
 	# serverStatusResult=client.admin.command("serverStatus")
 	# pprint(serverStatusResult)
 
-	print(list_dbs(client, mode='names'))
-	print(list_dbs(client, mode='cursors'))
-
-	
-
+	temp_db = create_db(client, "test2_database")
+	temp_db2 = create_db(client, "test3_database")
 	# temp_db = client["test2_database"]
 	# temp_db2 = client["test3_database"]
+
+	print(list_dbs(client, mode='names'))
+	drop_db(client, "test2_database")
+	drop_db(client, "test3_database")
+	# print(list_dbs(client, mode='cursors'))
+
+
+
 
 	
 	# posts = temp_db.posts #we need to use this .notation to make sure it's there 
