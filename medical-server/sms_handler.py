a9285a8fcefa8d8aa30a741960d2915ef2906ff6
Ted <tedkim97@gmail.com>
2019-08-14 13:34:41 -0500

    added TODO's with reminders for future work

--- medical-server/sms_handler.py
+++ medical-server/sms_handler.py
@@ -3,6 +3,7 @@ import json
 from databases import mongo_wrapper as mw
 import serial
 import threading
+import os
 
 #The ardunio should be flashed with the arduino file "sms_recv.ino"
 def sms_listener(port_name="/dev/ttyACM0", baud_rate=115200, ign=6, filter_key=["MESG::","FROM::"]):
@@ -10,7 +11,8 @@ def sms_listener(port_name="/dev/ttyACM0", baud_rate=115200, ign=6, filter_key=[
 
 	db_client = mw.open_connection('localhost',27017,'root',"humancomputerintegration")
 	collection = prepare_collection(db_client, "proto_test_db", "proto_test_coll")
-
+	umls_to_medt, ind_to_dumls, ind_to_sumls = process_dictionaries('medical_assets/DiseaseList.csv',
+																	'medical_assets/SymptomList.csv')
 
 	running = True
 	arduino_port = serial.Serial(port_name, baud_rate)
@@ -35,6 +37,31 @@ def sms_listener(port_name="/dev/ttyACM0", baud_rate=115200, ign=6, filter_key=[
 			info = tuple()
 
 	mw.close_connection(db_client)
+
+def process_dictionaries(umlsdis, umlssymp):
+	umls_to_medical_term = dict()
+	ind_to_dis_umls = dict()
+	ind_to_symp_umls = dict()
+
+	with open(umlsdis, "r") as f:
+		f.readline() #skip the header in the document
+		dis_counter = 0 
+		for line in f:
+			dis_pair = line.split(',')
+			ind_to_dis_umls[dis_counter] = dis_pair[1].strip()
+			umls_to_medical_term[dis_pair[0]] = dis_pair[1].strip()
+			dis_counter = dis_counter + 1
+
+	with open(umlssymp, "r") as g: 
+		g.readline() #skip the header in the document 
+		symp_counter = 0
+		for line in g:
+			symp_pair = line.split(',')
+			ind_to_symp_umls[symp_counter] = symp_pair[1].strip()
+			umls_to_medical_term[symp_pair[0]] = symp_pair[1].strip()
+			symp_counter = symp_counter + 1
+
+	return umls_to_medical_term, ind_to_dis_umls, ind_to_symp_umls
 
 
 def prepare_collection(client, dbname: str, coll_name:str):
@@ -69,3 +96,9 @@ def process_text(text):
 
 if __name__ == "__main__":
 	print("DOes nothing when called")
+	a, b, c = process_dictionaries('medical_assets/DiseaseList.csv','medical_assets/SymptomList.csv')
+	print(a)
+	print("------------------------")
+	print(b)
+	print("------------------------")
+	print(c)
