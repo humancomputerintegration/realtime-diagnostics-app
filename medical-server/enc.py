c6ce8883f5e008d82bdd4c451af0f21bcffa4bac
Ted Kim <tedkim97@cs.uchicago.edu>
2020-02-02 15:50:46 -0600

    changes to gitignore to ignore the privatekey

--- medical-server/enc.py
+++ medical-server/enc.py
@@ -1,19 +1,38 @@
 from Crypto.PublicKey import RSA
 
-def generate_keys(private_file, public_file, bsize=2048):
-    key = RSA.generate(bsi)
-    private_key = key.export_key()
-    file_out = open("private.pem", "wb")
-    file_out.write(private_key)
+def generate_keys(public_file='public.pem',private_file='private.pem', bsize=2048):
+    gen_key = RSA.generate(bsize)
+    prikey = gen_key.export_key()
+    file_out = open(private_file, "wb")
+    file_out.write(prikey)
 
-    public_key = key.publickey().export_key()
-    file_out = open("receiver.pem", "wb")
-    file_out.write(public_key)
-    return None
+    pubkey = gen_key.publickey().export_key()
+    file_out = open(public_file, "wb")
+    file_out.write(pubkey)
+    return gen_key
 
-def decrypt(enc_msg):
-    dec_msg = 
+def generate_keys_v2(public_file='public.pem',private_file='private.pem', bsize=2048):
+    gen_key = RSA.generate(bsize)
+    prikey = gen_key.export_key().decode()
+    file_out = open(private_file, "w")
+    file_out.write(prikey)
+
+    pubkey = gen_key.publickey().export_key().decode()
+    file_out = open(public_file, "w")
+    file_out.write(pubkey)
+    return gen_key
+
+def decrypt(enc_msg, private_key):
+    # dec_msg = 
+    
     return None
 
 if __name__ == "__main__":
-    print("this should be doing nothing")
+    test_msg = "this is a test message".encode('utf-8')
+    print(test_msg)
+    bkey = generate_keys('private.pem','public.pem',bsize=1024)
+    private_pem = bkey.export_key().decode()
+    public_pem = bkey.export_key().decode ()
+    print(private_pem)
+    print(public_pem)
+
